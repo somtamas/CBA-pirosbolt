d479d6f38abe19231a5dcd23aafd79d57398682b
somlyaitamas <somlyaitamas@turr.hu>
2026-06-01 09:26:29 +0200

    model

---
+++ CBA-pirosbolt/Model/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBA_pirosbolt.Model
+{
+    internal class Manager
+    {
+    }
+}
